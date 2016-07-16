b7464a5fabb17870ead179b0c4a4587dd5a91b9c
sebpearce <sebpearce@gmail.com>
2016-07-16 15:03:31 +1000

    add timezone

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module Expenses
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = "Melbourne"
+    config.active_record.default_timezone = :local
   end
 end
