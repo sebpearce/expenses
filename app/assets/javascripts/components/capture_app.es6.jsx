751a0cfb9c1f41ec41a313f42ca97f53bfdc882d
sebpearce <sebpearce@gmail.com>
2016-07-24 14:43:10 +1000

    Make capture component consume all data

--- app/assets/javascripts/components/capture_app.es6.jsx
+++ app/assets/javascripts/components/capture_app.es6.jsx
@@ -6,17 +6,29 @@ class CaptureApp extends React.Component {
     super(props);
     this.state = {
       expenseSources: [],
+      incomeSources: [],
+      expenseItems: [],
+      incomeItems: [],
+      settings: [],
+      shortcuts: [],
     };
   }
 
   loadDataFromServer () {
-    const source = 'http://localhost:3000/expense_sources';
+    const source = 'http://localhost:3000/capturejson';
 
     $.ajax({
       url: source,
       dataType: 'json',
       success: (data) => {
-        this.setState({expenseSources: data});
+        this.setState({
+          expenseSources: data.expense_sources,
+          incomeSources: data.income_sources,
+          expenseItems: data.expense_items,
+          incomeItems: data.income_items,
+          settings: data.settings,
+          shortcuts: data.shortcuts,
+        });
       },
       error: (xhr, status, err) => {
         console.error(source, status, err.toString());
@@ -30,7 +42,6 @@ class CaptureApp extends React.Component {
   }
 
   render () {
-
     return (
       <div>
         <p>CaptureApp</p>
