a69e40860db4c3a8dc9cd61e7a5d9de62c3a51d3
sebpearce <sebpearce@gmail.com>
2016-07-25 21:16:13 +1000

    Make all JSON responses into hashes

--- app/controllers/capture_json_controller.rb
+++ app/controllers/capture_json_controller.rb
@@ -6,8 +6,8 @@ class CaptureJsonController < ApplicationController
     render json: {
       expense_sources: format_sources_as_json(ExpenseSource.all),
       income_sources: format_sources_as_json(IncomeSource.all),
-      expense_items: format_items_as_json(ExpenseItem.all),
-      income_items: format_items_as_json(IncomeItem.all),
+      expense_items: format_expense_items_as_json(ExpenseItem.all),
+      income_items: format_income_items_as_json(IncomeItem.all),
       settings: format_settings_as_json(Setting.all),
       shortcuts: format_settings_as_json(Shortcut.all),
     }
@@ -16,12 +16,11 @@ class CaptureJsonController < ApplicationController
   private
 
   def format_settings_as_json(query)
-    query.to_a.map do |el|
-      {
-        name: el.name,
-        value: el.value,
-      }
+    res = {}
+    query.to_a.each do |el|
+      res[el.name] = el.value
     end
+    res
   end
 
   def format_sources_as_json(query)
@@ -32,15 +31,29 @@ class CaptureJsonController < ApplicationController
     res
   end
 
-  def format_items_as_json(query)
-    query.to_a.map do |el|
-    {
-      id: el.id,
-      date: el.date,
-      amt: el.amount,
-      desc: el.description,
-      src: el.source.id,
-    }
+  def format_expense_items_as_json(query)
+    res = {}
+    query.to_a.each do |el|
+      res[el.id] = {
+        date: el.date,
+        amt: el.amount,
+        desc: el.description,
+        src: el.expense_source_id,
+      }
     end
+    res
+  end
+
+  def format_income_items_as_json(query)
+    res = {}
+    query.to_a.each do |el|
+      res[el.id] = {
+        date: el.date,
+        amt: el.amount,
+        desc: el.description,
+        src: el.income_source_id,
+      }
+    end
+    res
   end
 end
